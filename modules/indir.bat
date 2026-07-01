447b5ede5e2dd3be977db95bc61fda139f75e7ee
lahmacunkebabi <eymenefeaydinlik5@gmail.com>
2026-07-01 16:13:01 +0300

    İndir modülü - manuel açma ile

--- modules/indir.bat
+++ modules/indir.bat
@@ -4,8 +4,8 @@ set /p link="Link: "
 set /p ad="Kaydedilecek isim (orn: dosya.zip): "
 echo Indiriliyor...
 powershell -Command "Invoke-WebRequest -Uri '%link%' -OutFile '%ad%'"
-echo Dosya cikariliyor...
+echo Dosya aciliyor...
 if not exist "Downloads" mkdir "Downloads"
-powershell -Command "Expand-Archive -Path '%ad%' -DestinationPath '.\Downloads' -Force"
-echo Islem tamam! 'Downloads' klasorune bakabilirsin.
+echo Acilıyor... (Windows Explorer ile)
+explorer "%ad%"
 pause
